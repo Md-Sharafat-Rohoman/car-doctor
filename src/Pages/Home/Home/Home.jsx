75b08c5e8d5bb14e41e789fc156eeb81fc75459b
unknown <mdsharafatrohomanzafourllah@gmail.com>
2024-01-16 19:35:18 +0600

    'such'

---
+++ src/Pages/Home/Home/Home.jsx
@@ -0,0 +1,16 @@
+import About from "../About/About";
+import Banner from "../Banner/Banner";
+import Serves from "../Serves/Serves";
+
+
+const Home = () => {
+    return (
+        <div>
+            <Banner></Banner>
+            <About></About>
+            <Serves></Serves>
+        </div>
+    );
+};
+
+export default Home;
